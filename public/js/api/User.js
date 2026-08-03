e42f31159ffdcafb3a2abfc20220f76ae3b9ef45
Ksenia Timokhina <kseenina@gmail.com>
2026-08-04 00:30:43 +0300

    API realized

--- public/js/api/User.js
+++ public/js/api/User.js
@@ -4,12 +4,14 @@
  * Имеет свойство URL, равное '/user'.
  * */
 class User {
+  static URL = '/user';
+  
   /**
    * Устанавливает текущего пользователя в
    * локальном хранилище.
    * */
   static setCurrent(user) {
-
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   /**
@@ -17,7 +19,7 @@ class User {
    * пользователе из локального хранилища.
    * */
   static unsetCurrent() {
-
+    localStorage.removeItem('user');
   }
 
   /**
@@ -25,7 +27,8 @@ class User {
    * из локального хранилища
    * */
   static current() {
-
+    const user = localStorage.getItem('user');
+    return user ? JSON.parse(user) : undefined;
   }
 
   /**
@@ -33,7 +36,18 @@ class User {
    * авторизованном пользователе.
    * */
   static fetch(callback) {
-
+    createRequest({
+      url: this.URL + '/current',
+      method: 'GET',
+      callback: (err, response) => {
+        if (response.success && response.user) {
+          this.setCurrent(response.user);
+        } else {
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -46,10 +60,9 @@ class User {
     createRequest({
       url: this.URL + '/login',
       method: 'POST',
-      responseType: 'json',
       data,
       callback: (err, response) => {
-        if (response && response.user) {
+        if (response.success && response.user) {
           this.setCurrent(response.user);
         }
         callback(err, response);
@@ -64,7 +77,17 @@ class User {
    * User.setCurrent.
    * */
   static register(data, callback) {
-
+    createRequest({
+      url: this.URL + '/register',
+      method: 'POST',
+      data,
+      callback: (err, response) => {
+        if (response.success && response.user) {
+          this.setCurrent(response.user);
+        }
+        callback(err, response);
+      }
+    });
   }
 
   /**
@@ -72,6 +95,15 @@ class User {
    * выхода необходимо вызвать метод User.unsetCurrent
    * */
   static logout(callback) {
-
+    createRequest({
+      url: this.URL + '/logout',
+      method: 'POST',
+      callback: (err, response) => {
+        if (response.success) {
+          this.unsetCurrent();
+        }
+        callback(err, response);
+      }
+    });
   }
 }
--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,39 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    let url = options.url;
+    const method = (options.method || 'GET').toUpperCase();
+    const data = options.data || {};
+    let sendData = null;
+    const callback = options.callback || function () {};
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    if (method === 'GET') {
+        const parts = [];
+        Object.keys(data).forEach(key => {
+            parts.push(key + '=' + data[key]);
+        });
+        if (parts.length > 0) {
+            url += '?' + parts.join('&');
+        }
+    }
+    else {
+        sendData = new FormData();
+        Object.keys(data).forEach(key => {
+            sendData.append(key, data[key]);
+        });
+    }
+    xhr.onload = () => {
+        callback(null, xhr.response);
+    };
+    xhr.onerror = () => {
+        callback(new Error('Ошибка сети'));
+    };
+    try {
+        xhr.open(method, url);
+        xhr.send(sendData)
+    }
+    catch(err) {
+        callback(err);
+    }
 };
